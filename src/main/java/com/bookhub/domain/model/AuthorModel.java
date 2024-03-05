d0b1eb6d2eb45da4a3b57a3282d69ece411ae439
Igor Oliveira <lrmcpe22@gmail.com>
2024-03-05 02:54:16 -0300

    removed all imports by '*'

--- src/main/java/com/bookhub/domain/model/AuthorModel.java
+++ src/main/java/com/bookhub/domain/model/AuthorModel.java
@@ -1,15 +1,22 @@
 package com.bookhub.domain.model;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import java.io.Serializable;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity(name = "author")
-public class AuthorModel {
+public class AuthorModel implements Serializable {
+
+    private static final long serialVersionUID = 1L;
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -20,5 +27,4 @@ public class AuthorModel {
 
     @Column(nullable = false)
     private String nationality;
-
 }
