efc51a73197f4e6a35a1320a18ebbc109648dd92
Igor Oliveira <lrmcpe22@gmail.com>
2024-03-05 01:18:06 -0300

    created book and author models

---
+++ src/main/java/com/bookhub/domain/model/AuthorModel.java
@@ -0,0 +1,24 @@
+package com.bookhub.domain.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity(name = "author")
+public class AuthorModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String nationality;
+
+}
---
+++ src/main/java/com/bookhub/domain/model/BookModel.java
@@ -0,0 +1,30 @@
+package com.bookhub.domain.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import java.io.Serializable;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity(name = "book")
+public class BookModel implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false)
+    private String isbn;
+
+    @ManyToOne
+    @JoinColumn(name = "author_id", nullable = false)
+    private AuthorModel author;
+}
